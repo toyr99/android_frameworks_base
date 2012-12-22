29156336b18523a1c8b9838ae0aea63e1146534e
JBirdVegas <jbirdvegas@gmail.com>
2012-12-21 23:52:55 -0500

    Avoid possible memory leaks by ensuring stream closures
    Stream close() methods should always be called in finally block
    to ensure we don't accidentally leak a stream.
    
    Time to fix some old sloppy code.
    
    Patch Set 2:
     + ensure closure of some other streams
    Patch Set 3:
     + missing import java.io.IOException
    Patch Set 4:
     + missing import
    Patch Set 5:
     + missing ';'
     + whitespace cleanup
    Patch Set 6:
     - Reverted changes to MemoryUsage (final requirement
         prevents my implentation)
     + fixed Object allocation in Root.java
    Patch Set 7:
     + fixed curly bracket style violations
    Patch Set 8:
     x fixed my own sloppy code :/
    
    Change-Id: I3460b558ed5b4489296388c9a52fbb773cefd31f
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/screenshot/GlobalScreenshot.java

--- services/java/com/android/server/DevicePolicyManagerService.java
+++ services/java/com/android/server/DevicePolicyManagerService.java
@@ -1071,6 +1071,14 @@ public class DevicePolicyManagerService extends IDevicePolicyManager.Stub {
                 // Ignore
             }
             journal.rollback();
+        } finally {
+            try {
+                if (stream != null) {
+                    stream.close();
+                }
+            } catch (IOException ex) {
+                // Ignore
+            }
         }
     }
 
--- wifi/java/android/net/wifi/WifiConfigStore.java
+++ wifi/java/android/net/wifi/WifiConfigStore.java
@@ -799,7 +799,8 @@ class WifiConfigStore {
                 if (out != null) {
                     try {
                         out.close();
-                    } catch (Exception e) {}
+                    } catch (Exception e) {
+                    }
                 }
 
                 //Quit if no more writes sent
@@ -943,7 +944,8 @@ class WifiConfigStore {
             if (in != null) {
                 try {
                     in.close();
-                } catch (Exception e) {}
+                } catch (Exception e) {
+                }
             }
         }
     }
