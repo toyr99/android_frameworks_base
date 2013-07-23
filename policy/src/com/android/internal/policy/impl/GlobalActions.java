9643aaed77e6c7d31b0ea7b873a60af8c0ace75a
Henrik Baard <henrik.baard@sonymobile.com>
2013-07-23 09:31:57 +0200

    GlobalAction dialog ref not set to null
    When a GlobalAction dialog is dismissed when screen goes into
    sleep the reference mDialog is not set to null. Next time the
    long presses power button and brings up the GlobalAction dialog
    the old reference still exist and we try to dismiss the dialog
    again even if it does not exist.
    
    The dialog reference in this case holds on to the dialog object
    using more memory than neccesary, and in rare cases we might get
    a crash when the dialog object is dissmissed the second time.
    
    Change-Id: I44ef1c385d90777e8f6dd9fe111eb3d9b0d3cf3f

--- policy/src/com/android/internal/policy/impl/GlobalActions.java
+++ policy/src/com/android/internal/policy/impl/GlobalActions.java
@@ -1163,6 +1163,7 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
             case MESSAGE_DISMISS:
                 if (mDialog != null) {
                     mDialog.dismiss();
+                    mDialog = null;
                 }
                 break;
             case MESSAGE_REFRESH:
