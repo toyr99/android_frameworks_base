b039225fd81db2e22e99cfca9ed2c8ac7d233e11
Dario Ferretti <ferrettidario@gmail.com>
2013-07-27 10:31:16 +0200

    Remove statusbar notif count.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2499,12 +2499,6 @@ public final class Settings {
         public static final String STATUS_BAR_BRIGHTNESS_CONTROL = "status_bar_brightness_control";
 
         /**
-         * Show the pending notification counts as overlays on the status bar
-         * @hide
-         */
-        public static final String STATUS_BAR_NOTIF_COUNT = "status_bar_notif_count";
-
-        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
