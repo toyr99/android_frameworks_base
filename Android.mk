3eeefccf185ba8ad88aedfd20696467587321e4f
Faryaab Sheikh <faryaabsheikh@gmail.com>
2013-08-09 15:24:07 +0500

    Fix build

--- Android.mk
+++ Android.mk
@@ -232,7 +232,7 @@ LOCAL_SRC_FILES += \
 	telephony/java/com/android/internal/telephony/ITelephonyRegistry.aidl \
 	telephony/java/com/android/internal/telephony/IWapPushManager.aidl \
 	wifi/java/android/net/wifi/IWifiManager.aidl \
-	wifi/java/android/net/wifi/p2p/IWifiP2pManager.aidl
+	wifi/java/android/net/wifi/p2p/IWifiP2pManager.aidl \
 	fmradio/java/com/stericsson/hardware/fm/IFmReceiver.aidl \
 	fmradio/java/com/stericsson/hardware/fm/IFmTransmitter.aidl \
 	fmradio/java/com/stericsson/hardware/fm/IOnStateChangedListener.aidl \
