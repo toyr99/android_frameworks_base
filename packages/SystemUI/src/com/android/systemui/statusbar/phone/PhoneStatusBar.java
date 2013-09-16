5cacad8f37e1e95c2a5bc831a01ded537b094149
Phil Tunstall <ptunstall@gmail.com>
2013-09-16 16:08:51 +0100

    Fix Quick Settings tiles jerking down while animating to notifications view
    Patch Set 2: Fixed in code rather than XML
    
    cherry-picked to cm-11: fixes Quick Access Ribbons not appearing
    when switching back from Quick Settings
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
    
    Change-Id: I1689aa80a0fcb8ad17bcefc290b43a16079f7b88

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -1991,6 +1991,17 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode {
         return a;
     }
 
+    public Animator setVisibilityOnStart(
+            final Animator a, final View v, final int vis) {
+        a.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+                v.setVisibility(vis);
+            }
+        });
+        return a;
+    }
+
     public Animator interpolator(TimeInterpolator ti, Animator a) {
         a.setInterpolator(ti);
         return a;
@@ -2060,9 +2071,11 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode {
 	if (mRibbonView != null && mHasQuickAccessSettings) {
             mRibbonViewAnim = start(
                 startDelay(FLIP_DURATION_OUT * zeroOutDelays,
+                    setVisibilityOnStart(
                         interpolator(mDecelerateInterpolator,
                             ObjectAnimator.ofFloat(mRibbonView, View.SCALE_X, 1f)
-                                .setDuration(FLIP_DURATION_IN))));
+                                .setDuration(FLIP_DURATION_IN)),
+                        mRibbonView, View.VISIBLE)));
         }
         mFlipSettingsViewAnim = start(
             setVisibilityWhenDone(
