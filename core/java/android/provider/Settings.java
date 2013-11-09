300f8395da9353f063155512ae1a9c07856ad168
Lars Greiss <kufikugel@googlemail.com>
2013-11-09 05:01:26 +0100

    Frameworks: Slims ElectronBeam get some love (1/2)
    Features:
    - You can select different crt off animation modes if device overlay does not disable it at all
    - disabled
    - always horizontal (stock behaviour)
    - always vertical (screen collapse from left and right to the middle)
    - orientation dependent (means portrait = horizontal animation, landscape = vertical animation)
    - some minor fixes/code style improvements to 4.3 implementation
    
    Change-Id: I02543a242f8f570438f2ab97a19b6ffe1b4dba1c

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3743,6 +3743,16 @@ public final class Settings {
         public static final String SCREEN_LOCK_SLIDE_SCREENOFF_DELAY = "screen_lock_slide_screenoff_delay";
 
         /**
+         * Electronic beam animation mode
+         * 0 = off,
+         * 1 = always horizontal,
+         * 2 = always vertical,
+         * 3 = dependent on orientation
+         * @hide
+         */
+        public static final String SYSTEM_POWER_CRT_MODE = "system_power_crt_mode";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
