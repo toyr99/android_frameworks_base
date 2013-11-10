0dee98a624877bd18fdb3b283bbb7e48e8143c87
maxwen <max.weninger@gmail.com>
2013-11-10 13:17:37 +0100

    base: show notification count overlay
    Change-Id: Ie4df927531bd8d2869780336b5657f8194141b42

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4060,6 +4060,12 @@ public final class Settings {
         public static final String LOCKSCREEN_SEE_THROUGH = "lockscreen_see_through";
 
         /**
+         * Show the pending notification counts as overlays on the status bar
+         * @hide
+         */
+        public static final String STATUS_BAR_NOTIF_COUNT = "status_bar_notif_count";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
