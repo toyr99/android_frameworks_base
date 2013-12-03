ae2d1e93326c7a3455ae01f3660a5c1e7240d4d8
Steve Kondik <shade@chemlab.org>
2013-12-03 07:04:30 -0800

    Register the ActivityTrigger on all QCOM devices
    Change-Id: Iaecb5dd82be64fea5a625cc42243203d0f2f2ce9
    
    Conflicts:
    
    	core/jni/AndroidRuntime.cpp

--- core/jni/AndroidRuntime.cpp
+++ core/jni/AndroidRuntime.cpp
@@ -1221,7 +1221,8 @@ static const RegJNIRec gRegJNI[] = {
     REG_JNI(register_android_animation_PropertyValuesHolder),
     REG_JNI(register_com_android_internal_content_NativeLibraryHelper),
     REG_JNI(register_com_android_internal_net_NetworkStatsFactory),
-#ifdef QCOM_HARDWARE
+    REG_JNI(register_android_content_res_PackageRedirectionMap),
+#ifdef QCOM_ACTIVITY_TRIGGER
     REG_JNI(register_com_android_internal_app_ActivityTrigger),
 #endif
 };
