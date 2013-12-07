c5a01d46aab588d2a503f20beed1f6f9ca0fad47
Mahdi-Rom <mahdi.rom@gmail.com>
2013-12-07 07:53:21 +0000

    Activedisplay: fix SystemUI force close.

--- services/java/com/android/server/NotificationManagerService.java
+++ services/java/com/android/server/NotificationManagerService.java
@@ -805,14 +805,13 @@ public class NotificationManagerService extends INotificationManager.Stub
         for (NotificationListenerInfo info : toRemove) {
             final ComponentName component = info.component;
             final int oldUser = info.userid;
-            Slog.v(TAG, "disabling notification listener for user " + oldUser + ": " + component);
-            // Do not un-register HALO, we un-register only when HALO is closed
-            // Halo
-	    if (!component.getPackageName().equals("HaloComponent")) unregisterListenerService(component, info.userid);
+            Slog.v(TAG, "disabling notification listener for user " + oldUser + ": " + component);            	    
 	    // Active display
-	    if (!info.isSystem) {
-                Slog.v(TAG, "disabling notification listener for user " + oldUser + ": " + component);
+	    if (!info.isSystem) {                
                 unregisterListenerService(component, info.userid);
+	    // Halo
+	    // Do not un-register HALO, we un-register only when HALO is closed
+	    if (!component.getPackageName().equals("HaloComponent")) unregisterListenerService(component, info.userid);
             }        
 	}
 
@@ -833,14 +832,14 @@ public class NotificationManagerService extends INotificationManager.Stub
      */
     @Override
     public void registerListener(final INotificationListener listener,
-            final ComponentName component, final int userid) {
-	// Halo
-	if (!component.getPackageName().equals("HaloComponent")) checkCallerIsSystem();        
+            final ComponentName component, final int userid) {	       
 	// Active display	
 	final int permission = mContext.checkCallingPermission(
                 android.Manifest.permission.SYSTEM_NOTIFICATION_LISTENER);
         if (permission == PackageManager.PERMISSION_DENIED) {
             checkCallerIsSystem();
+	// Halo
+	if (!component.getPackageName().equals("HaloComponent")) checkCallerIsSystem(); 
 	}
         
         synchronized (mNotificationList) {
