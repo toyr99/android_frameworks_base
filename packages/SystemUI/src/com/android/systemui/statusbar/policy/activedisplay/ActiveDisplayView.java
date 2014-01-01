72f4271db48821df602c73e0c998cb039fd0bb2f
Mahdi-Rom <mahdi.rom@gmail.com>
2014-01-01 17:41:06 +0000

    Revert "base: ActiveDisplay: fix for disappearing notification bar"
    This reverts commit ad19c41b0d8529470ea774a6a7436a3929cc0361.

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/activedisplay/ActiveDisplayView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/activedisplay/ActiveDisplayView.java
@@ -665,9 +665,6 @@ public class ActiveDisplayView extends FrameLayout {
         setVisibility(View.GONE);
         restoreBrightness();
         mWakedByPocketMode = false;
-        if (!mKeyguardManager.isKeyguardSecure()) {
-            mBar.disable(0);
-        }
         cancelTimeoutTimer();
         unregisterSensorListener(mLightSensor);
     }
