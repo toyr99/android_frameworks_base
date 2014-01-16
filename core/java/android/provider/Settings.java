3349400d1bd5bd8f062dd2a2224c98600d027065
David96 <xd_e-mail@gmx.de>
2014-01-16 16:16:48 +0530

    [1/2] Base: Lockscreen blur
    Show the content behind lockscreen, blurred.
    
    -78915a8 Base: Lockscreen blur fixes
    -also clean up KeyguardViewManager.java of some old code that shoud not have been there

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4018,6 +4018,12 @@ public final class Settings {
         public static final String WAKELOCK_BLOCKING_LIST = "wakelock_blocking_list";
 
         /**
+         * Allows to show the background activity back the lockscreen
+         * @hide
+         */
+        public static final String LOCKSCREEN_SEE_THROUGH = "lockscreen_see_through";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
