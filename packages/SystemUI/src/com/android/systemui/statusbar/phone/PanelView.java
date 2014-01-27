d22f1a6181ffcd5e1db1c5af3c4653db080ec071
PrimeDirective <activethrasher00@gmail.com>
2014-01-27 02:29:39 -0500

    Silence the debuggers squashed...
    wifistatetracker: silence more logging
    
    Change-Id: I3d8e7cf0ed028b1213149d89088a2021c838e433
    
    notification/settingspanel: stop getting a gesture recorder
    
    We're currently getting gesture information on fling even
    if the log ouput for DEBUG_GESTURES is turned off so.. lets stop.
    
    -Vanir
    
    Change-Id: I70810fb2fb9e4b134492ffdd1ac844fe65f7fb75
    
    systemui: turn off debugging
    
    should be all of it
    
    -VanirAOSP
    
    Change-Id: Ia279a1be6968f67a4ad448cf51743f4c455af88a
    
    SearchPanelView: disable gesture debugging
    
    Change-Id: I7ce5e2420fa7a1a3f9533ea2e04cd94e13939724

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PanelView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PanelView.java
@@ -38,7 +38,7 @@ public class PanelView extends FrameLayout {
     public static final boolean DEBUG = PanelBar.DEBUG;
     public static final String TAG = PanelView.class.getSimpleName();
 
-    public static final boolean DEBUG_NAN = true; // http://b/7686690
+    public static final boolean DEBUG_NAN = false; // http://b/7686690
 
     private final void logf(String fmt, Object... args) {
         Log.v(TAG, (mViewName != null ? (mViewName + ": ") : "") + String.format(fmt, args));
@@ -316,8 +316,10 @@ public class PanelView extends FrameLayout {
                 post(mStopAnimator);
             }
         } else {
-            Log.v(TAG, "animationTick called with dtms=" + dtms + "; nothing to do (h="
-                    + mExpandedHeight + " v=" + mVel + ")");
+            if (DEBUG) {
+                Log.v(TAG, "animationTick called with dtms=" + dtms + "; nothing to do (h="
+                        + mExpandedHeight + " v=" + mVel + ")");
+            }
         }
     }
 
