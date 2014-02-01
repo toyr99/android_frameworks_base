93673163e0f350208d43393cf8b74fb55549b745
Mahdi-Rom <mahdi.rom@gmail.com>
2014-02-01 03:53:06 +0000

    Lockscreen Notifications: revert all navbar changes

--- packages/Keyguard/src/com/android/keyguard/KeyguardViewMediator.java
+++ packages/Keyguard/src/com/android/keyguard/KeyguardViewMediator.java
@@ -128,7 +128,6 @@ public class KeyguardViewMediator {
     private static final int LAUNCH_CAMERA = 16;
     private static final int DISMISS = 17;
     private static final int START_CUSTOM_INTENT = 18;
-    private static final int DISPATCH_BUTTON_CLICK_EVENT = 19;
 
     /**
      * The default amount of time we stay awake (used for all key input)
@@ -1155,9 +1154,6 @@ public class KeyguardViewMediator {
                 case DISPATCH_EVENT:
                     handleDispatchEvent((MotionEvent) msg.obj);
                     break;
-                case DISPATCH_BUTTON_CLICK_EVENT:
-                    handleDispatchButtonClickEvent(msg.arg1);
-                    break;
                 case LAUNCH_CAMERA:
                     handleLaunchCamera();
                     break;
@@ -1206,10 +1202,6 @@ public class KeyguardViewMediator {
 
     protected void handleDispatchEvent(MotionEvent event) {
         mKeyguardViewManager.dispatch(event);
-    }
-
-    protected void handleDispatchButtonClickEvent(int buttonId) {
-        mKeyguardViewManager.dispatchButtonClick(buttonId);
     }
 
     private void sendUserPresentBroadcast() {
@@ -1455,12 +1447,6 @@ public class KeyguardViewMediator {
 
     public void dispatch(MotionEvent event) {
         Message msg = mHandler.obtainMessage(DISPATCH_EVENT, event);
-        mHandler.sendMessage(msg);
-    }
-
-    public void dispatchButtonClick(int buttonId) {
-        Message msg = mHandler.obtainMessage(DISPATCH_BUTTON_CLICK_EVENT);
-        msg.arg1 = buttonId;
         mHandler.sendMessage(msg);
     }
 
