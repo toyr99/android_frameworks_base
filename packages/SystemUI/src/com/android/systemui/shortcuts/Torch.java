4afc77e2525fb14a2cf8ca4792783c0172c04384
Jubakuba <Jubakuba@gmail.com>
2014-02-09 09:16:18 -0700

    SystemUI: Actions to Shortcuts (1/2)
    Change-Id: Ic7cc70b5a55e98b1fd6784380049f54d0bd3f4d2

--- packages/SystemUI/src/com/android/systemui/shortcuts/Torch.java
+++ packages/SystemUI/src/com/android/systemui/shortcuts/Torch.java
@@ -19,9 +19,11 @@ package com.android.systemui.shortcuts;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.UserHandle;
 
 import com.android.internal.util.mahdi.ButtonsConstants;
 import com.android.internal.util.mahdi.MahdiActions;
+import com.android.internal.util.mahdi.TorchConstants;
 
 public class Torch extends Activity  {
 
@@ -33,8 +35,21 @@ public class Torch extends Activity  {
     @Override
     public void onResume() {
         super.onResume();
-        MahdiActions.processAction(
-                this, ButtonsConstants.ACTION_TORCH, false);
+        int value = getIntent().getIntExtra("value", 2);
+        Intent intent = new Intent();
+        switch (value) {
+            case 0:
+                intent.setAction(TorchConstants.ACTION_OFF);
+                break;
+            case 1:
+                intent.setAction(TorchConstants.ACTION_ON);
+                break;
+            case 2:
+                intent.setAction(TorchConstants.ACTION_TOGGLE_STATE);
+                break;
+        }
+        this.sendBroadcastAsUser(
+                intent, new UserHandle(UserHandle.USER_CURRENT));
         this.finish();
     }
 }
