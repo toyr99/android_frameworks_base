SlimRoms/frameworks_base

SystemUI: Extra Slim Shortcuts && Cleanup
Some cleanup of slim shortcuts
	& implement method to allow
	user to toggle toggleable
	settings to the preferred state.
	Much easier to use with confidence
	knowing you'll be in the correct state.

Change-Id: I897ee4567426d0314952b9860a487ab445795dfc

packages/SystemUI/src/com/android/systemui/shortcuts/Immersive.java
@@ -19,9 +19,8 @@ package com.android.systemui.shortcuts;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-
-import com.android.internal.util.slim.PolicyConstants;
-import com.android.internal.util.slim.SlimActions;
+import android.os.UserHandle;
+import android.provider.Settings;
 
 public class Immersive extends Activity  {
 
@@ -33,8 +32,19 @@ public class Immersive extends Activity  {
     @Override
     public void onResume() {
         super.onResume();
-        SlimActions.processAction(
-                this, PolicyConstants.ACTION_EXPANDED_DESKTOP, false);
+        int value = getIntent().getIntExtra("value", 2);
+
+        if (value == 2) {
+            value = Settings.System.getIntForUser(
+                    getContentResolver(),
+                    Settings.System.EXPANDED_DESKTOP_STATE,
+                    0, UserHandle.USER_CURRENT_OR_SELF) == 1 ? 0 : 1;
+        }
+
+        Settings.System.putIntForUser(
+                getContentResolver(),
+                Settings.System.EXPANDED_DESKTOP_STATE,
+                value, UserHandle.USER_CURRENT_OR_SELF);
         this.finish();
     }
 }

packages/SystemUI/src/com/android/systemui/shortcuts/PowerMenu.java
@@ -17,12 +17,12 @@
 package com.android.systemui.shortcuts;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 
-import com.android.internal.view.RotationPolicy;
+import com.android.internal.util.slim.ButtonsConstants;
+import com.android.internal.util.slim.SlimActions;
 
-public class Rotation extends Activity  {
+public class PowerMenu extends Activity  {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -32,16 +32,7 @@ public class Rotation extends Activity  {
     @Override
     public void onResume() {
         super.onResume();
-        int value = getIntent().getIntExtra("value", 2);
-
-        boolean userRotation;
-        if (value == 2) {
-            userRotation = RotationPolicy.isRotationLocked(this);
-        } else {
-            userRotation = value == 1;
-        }
-
-        RotationPolicy.setRotationLock(this, !userRotation);
+        SlimActions.processAction(this, ButtonsConstants.ACTION_POWER_MENU, false);
         this.finish();
     }
 }

packages/SystemUI/src/com/android/systemui/shortcuts/Screenshot.java
@@ -17,8 +17,8 @@
 package com.android.systemui.shortcuts;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import com.android.internal.util.slim.ButtonsConstants;
 import com.android.internal.util.slim.SlimActions;
@@ -33,8 +33,14 @@ public class Screenshot extends Activity  {
     @Override
     public void onResume() {
         super.onResume();
-        SlimActions.processAction(
-                this, ButtonsConstants.ACTION_SCREENSHOT, false);
-        this.finish();
+        Handler handle = new Handler();
+        // Allow statusbar to collapse if desired
+        handle.postDelayed(new Runnable() {
+            public void run() {
+                SlimActions.processActionWithOptions(Screenshot.this,
+                        ButtonsConstants.ACTION_SCREENSHOT, false, false);
+                Screenshot.this.finish();
+            }
+        }, 500);
     }
 }

toyr99/android_frameworks_base

packages/SystemUI/src/com/android/systemui/shortcuts/Immersive.java
@@ -19,6 +19,8 @@ package com.android.systemui.shortcuts;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.UserHandle;
+import android.provider.Settings;
 
 import com.android.internal.util.mahdi.MahdiActions;
 import com.android.internal.util.mahdi.ButtonsConstants;
@@ -33,8 +35,19 @@ public class Immersive extends Activity  {
     @Override
     public void onResume() {
         super.onResume();
-        MahdiActions.processAction(
-                this, ButtonsConstants.ACTION_IMMERSIVE_MODE, false);
+        int value = getIntent().getIntExtra("value", 2);
+
+        if (value == 2) {
+            value = Settings.System.getIntForUser(
+                    getContentResolver(),
+                    Settings.System.GLOBAL_IMMERSIVE_MODE_STATE,
+                    0, UserHandle.USER_CURRENT_OR_SELF) == 1 ? 0 : 1;
+        }
+
+        Settings.System.putIntForUser(
+                getContentResolver(),
+                Settings.System.GLOBAL_IMMERSIVE_MODE_STATE,
+                value, UserHandle.USER_CURRENT_OR_SELF);
         this.finish();
     }
 }

packages/SystemUI/src/com/android/systemui/shortcuts/PowerMenu.java
@@ -17,12 +17,12 @@
 package com.android.systemui.shortcuts;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 
-import com.android.internal.view.RotationPolicy;
+import com.android.internal.util.mahdi.ButtonsConstants;
+import com.android.internal.util.mahdi.MahdiActions;
 
-public class Rotation extends Activity  {
+public class PowerMenu extends Activity  {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -32,16 +32,7 @@ public class Rotation extends Activity  {
     @Override
     public void onResume() {
         super.onResume();
-        int value = getIntent().getIntExtra("value", 2);
-
-        boolean userRotation;
-        if (value == 2) {
-            userRotation = RotationPolicy.isRotationLocked(this);
-        } else {
-            userRotation = value == 1;
-        }
-
-        RotationPolicy.setRotationLock(this, !userRotation);
+        MahdiActions.processAction(this, ButtonsConstants.ACTION_POWER_MENU, false);
         this.finish();
     }
 }

packages/SystemUI/src/com/android/systemui/shortcuts/Screenshot.java
@@ -17,8 +17,8 @@
 package com.android.systemui.shortcuts;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import com.android.internal.util.mahdi.ButtonsConstants;
 import com.android.internal.util.mahdi.MahdiActions;
@@ -33,8 +33,14 @@ public class Screenshot extends Activity  {
     @Override
     public void onResume() {
         super.onResume();
-        MahdiActions.processAction(
-                this, ButtonsConstants.ACTION_SCREENSHOT, false);
-        this.finish();
+        Handler handle = new Handler();
+        // Allow statusbar to collapse if desired
+        handle.postDelayed(new Runnable() {
+            public void run() {
+                MahdiActions.processActionWithOptions(Screenshot.this,
+                        ButtonsConstants.ACTION_SCREENSHOT, false, false);
+                Screenshot.this.finish();
+            }
+        }, 500);
     }
 }