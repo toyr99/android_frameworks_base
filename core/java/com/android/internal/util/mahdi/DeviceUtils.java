SlimRoms/frameworks_base

Frameworks: Smart PullDown to SlimActions (1/2)
PS2: Dependant on bug fixes

Change-Id: I8023933cb064429aef84e4a027645108e18d2940

core/java/com/android/internal/util/slim/DeviceUtils.java
@@ -127,7 +127,9 @@ public class DeviceUtils {
                 || action.equals(ButtonsConstants.ACTION_VIB)
                         && !deviceSupportsVibrator(context)
                 || action.equals(ButtonsConstants.ACTION_VIB_SILENT)
-                        && !deviceSupportsVibrator(context)) {
+                        && !deviceSupportsVibrator(context)
+                || action.equals(ButtonsConstants.ACTION_SMART_PULLDOWN)
+                        && isTablet(context)) {
             return false;
         }
         return true;

toyr99/android_frameworks_base

Conflicts:
	core/java/com/android/internal/util/mahdi/DeviceUtils.java
	core/java/com/android/internal/util/mahdi/MahdiActions.java
	packages/SystemUI/res/values/slim_strings.xml

core/java/com/android/internal/util/mahdi/DeviceUtils.java
@@ -137,7 +137,10 @@ public class DeviceUtils {
     }
 
     private static boolean isSupportedFeature(Context context, String action) {
-        if (action.equals(ButtonsConstants.ACTION_TORCH) && !deviceSupportsTorch(context)) {
+        if (action.equals(ButtonsConstants.ACTION_TORCH) 
+                        && !deviceSupportsTorch(context)
+                || action.equals(ButtonsConstants.ACTION_SMART_PULLDOWN)
+                        && isTablet(context)) {
             return false;
         }
         return true;