SlimRoms/frameworks_base

Framework: All Shake Patches (1/X)
Included:
	Keyguard - Disable Shake if isHidden
	Disabled Shake Apps (1/2)
	Shake Sensitivity (1/2)
	Keyguard Shake Events (1/2)
	Shake To Perform Event (1/2)
	430942 miscellaneous fixes

Change-Id: I47707f51e61ba0d4687e0232d684de7ee6b0eb72

packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -107,6 +107,8 @@ import com.android.internal.util.slim.ButtonConfig;
 import com.android.internal.util.slim.ButtonsConstants;
 import com.android.internal.util.slim.ButtonsHelper;
 import com.android.internal.util.slim.DeviceUtils;
+import com.android.internal.util.slim.ShakeListener;
+import com.android.internal.util.slim.SlimActions;
 
 import com.android.systemui.BatteryMeterView;
 import com.android.systemui.BatteryCircleMeterView;
@@ -205,6 +207,9 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
     BatteryController mBatteryController;
     LocationController mLocationController;
     NetworkController mNetworkController;
+
+    // Shake listener for user-defined events
+    private ShakeListener mShakeListener;
 
     int mNaturalBarHeight = -1;
     int mIconSize = -1;
@@ -325,6 +330,9 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
     private FrameLayout.LayoutParams lpCarrierLabel;
     private int mShortcutsDrawerMargin;
     private int mShortcutsSpacingHeight;
+
+    private boolean mShakeEnabled;
+    private boolean mUserPresent;
 
     // drag bar
     private int mCloseViewHeight;
@@ -515,6 +523,12 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.VOLUME_KEY_CURSOR_CONTROL), false, this,
                     UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.SHAKE_LISTENER_ENABLED), false, this,
+                    UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.SHAKE_SENSITIVITY), false, this,
+                    UserHandle.USER_ALL);
             update();
         }
 
@@ -628,6 +642,12 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
             } else if (uri.equals(Settings.System.getUriFor(
                     Settings.System.EXPANDED_DESKTOP_STATE))) {
                 mNavigationBarOverlay.setIsExpanded(isExpanded());
+            } else if (uri.equals(Settings.System.getUriFor(
+                    Settings.System.SHAKE_LISTENER_ENABLED))) {
+                updateShakeListener();
+            } else if (uri.equals(Settings.System.getUriFor(
+                    Settings.System.SHAKE_SENSITIVITY))) {
+                updateShakeSensitivity();
             }
 
             update();
@@ -1365,6 +1385,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         filter.addAction(Intent.ACTION_CLOSE_SYSTEM_DIALOGS);
         filter.addAction(Intent.ACTION_SCREEN_OFF);
         filter.addAction(Intent.ACTION_SCREEN_ON);
+        filter.addAction(Intent.ACTION_USER_PRESENT);
         filter.addAction(ACTION_DEMO);
         filter.addAction(SCHEDULE_REMINDER_NOTIFY);
         context.registerReceiver(mBroadcastReceiver, filter);
@@ -1389,6 +1410,8 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         updateBatteryIcons();
 
         mNetworkController.setListener(this);
+
+        updateShakeListener();
 
         return mStatusBarView;
     }
@@ -2225,6 +2248,92 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         } else if ((diff & StatusBarManager.DISABLE_NOTIFICATION_TICKER) != 0) {
             if (mTicking && (state & StatusBarManager.DISABLE_NOTIFICATION_TICKER) != 0) {
                 haltTicker();
+            }
+        }
+    }
+
+    private void unregisterShakeListener() {
+        if (mShakeEnabled) {
+            getShakeListener().unregisterShakeListener();
+        }
+    }
+
+    private void registerShakeListener() {
+        if (mShakeEnabled) {
+            getShakeListener().registerShakeListener();
+        }
+    }
+
+    private void updateShakeListener() {
+        mShakeEnabled = Settings.System.getIntForUser(
+                mContext.getContentResolver(),
+                Settings.System.SHAKE_LISTENER_ENABLED, 0,
+                UserHandle.USER_CURRENT) == 1;
+        if (mShakeEnabled) {
+            getShakeListener().setOnShakeListener(new ShakeListener.OnShakeListener() {
+                public void onShake(int direction) {
+                    performShakeEvent(direction);
+                }
+            });
+            updateShakeSensitivity();
+            if ((mScreenOn == null || mScreenOn) && mUserPresent) {
+                mShakeListener.registerShakeListener();
+            }
+        } else {
+            getShakeListener().unregisterShakeListener();
+        }
+    }
+
+    private boolean shakeAppDisabled() {
+        ActivityManager am = (ActivityManager)
+                mContext.getSystemService(Context.ACTIVITY_SERVICE);
+        final String thisPackage = am.getRunningTasks(1)
+                .get(0).topActivity.getPackageName();
+        String resolvedApp = null;
+        boolean shakeDisabled = false;
+        for (ButtonConfig app : ButtonsHelper.getDisabledShakeApps(mContext)) {
+            resolvedApp = app.getClickAction();
+            resolvedApp = resolvedApp.substring(
+                    resolvedApp.lastIndexOf("component=") + 10,
+                    resolvedApp.lastIndexOf("/"));
+            if (resolvedApp.equals(thisPackage)) {
+                shakeDisabled = true;
+            }
+        }
+        return shakeDisabled;
+    }
+
+    private void updateShakeSensitivity() {
+        getShakeListener().setSensitivity(true, Settings.System.getIntForUser(
+                mContext.getContentResolver(),
+                Settings.System.SHAKE_SENSITIVITY, 0, UserHandle.USER_CURRENT));
+    }
+
+    private ShakeListener getShakeListener() {
+        if (mShakeListener == null) {
+            mShakeListener = new ShakeListener(mContext);
+        }
+        return mShakeListener;
+    }
+
+    private void performShakeEvent(int setting) {
+        if (mShakeEnabled && !shakeAppDisabled()) {
+            if (mCurrOrientation == Configuration.ORIENTATION_LANDSCAPE) {
+                // X and Y values are swapped in landscape for ease of
+                // understanding which event will be launched by the user
+                if (setting == 0) {
+                    setting = 1;
+                } else if (setting == 1) {
+                    setting = 0;
+                }
+            }
+            final String event = Settings.System.getStringForUser(
+                    mContext.getContentResolver(),
+                    Settings.System.SHAKE_EVENTS_REGULAR[setting],
+                    UserHandle.USER_CURRENT);
+            if (event != null && !event.equals(ButtonsConstants.ACTION_NULL)) {
+                customButtonVibrate();
+                SlimActions.processAction(mContext, event, false);
             }
         }
     }
@@ -3691,16 +3800,21 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                 animateCollapsePanels(flags);
             } else if (Intent.ACTION_SCREEN_OFF.equals(action)) {
                 mScreenOn = false;
+                mUserPresent = false;
                 // no waiting!
                 makeExpandedInvisible();
                 notifyNavigationBarScreenOn(false);
                 notifyHeadsUpScreenOn(false);
+                unregisterShakeListener();
                 finishBarAnimations();
             } else if (Intent.ACTION_SCREEN_ON.equals(action)) {
                 mScreenOn = true;
                 // work around problem where mDisplay.getRotation() is not stable while screen is off (bug 7086018)
                 repositionNavigationBar();
                 notifyNavigationBarScreenOn(true);
+            } else if (Intent.ACTION_USER_PRESENT.equals(action)) {
+                mUserPresent = true;
+                registerShakeListener();
             } else if (SCHEDULE_REMINDER_NOTIFY.equals(action)) {
                 updateAndNotifyReminder();
             } else if (ACTION_DEMO.equals(action)) {

packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsController.java
@@ -43,6 +43,7 @@ import static com.android.internal.util.slim.QSConstants.TILE_QUIETHOURS;
 import static com.android.internal.util.slim.QSConstants.TILE_RINGER;
 import static com.android.internal.util.slim.QSConstants.TILE_SCREENTIMEOUT;
 import static com.android.internal.util.slim.QSConstants.TILE_SETTINGS;
+import static com.android.internal.util.slim.QSConstants.TILE_SHAKE;
 import static com.android.internal.util.slim.QSConstants.TILE_SLEEP;
 import static com.android.internal.util.slim.QSConstants.TILE_SYNC;
 import static com.android.internal.util.slim.QSConstants.TILE_THEME;
@@ -95,6 +96,7 @@ import com.android.systemui.quicksettings.QuietHoursTile;
 import com.android.systemui.quicksettings.RingerModeTile;
 import com.android.systemui.quicksettings.ScreenTimeoutTile;
 import com.android.systemui.quicksettings.SleepScreenTile;
+import com.android.systemui.quicksettings.ShakeEventTile;
 import com.android.systemui.quicksettings.SyncTile;
 import com.android.systemui.quicksettings.ThemeTile;
 import com.android.systemui.quicksettings.ToggleLockscreenTile;
@@ -251,6 +253,8 @@ public class QuickSettingsController {
                 qs = new ThemeTile(mContext, this);
             } else if (tile.equals(TILE_QUICKRECORD)) {
                 qs = new QuickRecordTile(mContext, this);
+            } else if (tile.equals(TILE_SHAKE)) {
+                qs = new ShakeEventTile(mContext, this);
             } else if (tile.contains(TILE_CUSTOM)) {
                 qs = new CustomTile(mContext, this, findCustomKey(tile));
             } else if (tile.contains(TILE_CONTACT)) {

toyr99/android_frameworks_base

Change-Id: I47707f51e61ba0d4687e0232d684de7ee6b0eb

packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -112,6 +112,8 @@ import com.android.internal.util.mahdi.ButtonConfig;
 import com.android.internal.util.mahdi.ButtonsConstants;
 import com.android.internal.util.mahdi.ButtonsHelper;
 import com.android.internal.util.mahdi.DeviceUtils;
+import com.android.internal.util.mahdi.MahdiActions;
+import com.android.internal.util.slim.ShakeListener;
 
 import com.android.systemui.BatteryMeterView;
 import com.android.systemui.BatteryMeterView.BatteryMeterMode;
@@ -220,6 +222,9 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
     BatteryController mBatteryController;
     LocationController mLocationController;
     NetworkController mNetworkController;
+
+    // Shake listener for user-defined events
+    private ShakeListener mShakeListener;
 
     int mNaturalBarHeight = -1;
     int mIconSize = -1;
@@ -361,6 +366,9 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
     private FrameLayout.LayoutParams lpCarrierLabel;
     private int mShortcutsDrawerMargin;
     private int mShortcutsSpacingHeight;
+
+    private boolean mShakeEnabled;
+    private boolean mUserPresent;
 
     // drag bar
     private int mCloseViewHeight;
@@ -541,6 +549,12 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.VOLUME_KEY_CURSOR_CONTROL), false, this,
                     UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.SHAKE_LISTENER_ENABLED), false, this,
+                    UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.SHAKE_SENSITIVITY), false, this,
+                    UserHandle.USER_ALL);
             updateSettings();
         }
 
@@ -595,7 +609,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                     Settings.System.LOCKSCREEN_USE_WIDGET_CONTAINER_CAROUSEL))) {
                 if (mNavigationBarView != null) {
                     mNavigationBarView.disableCameraByUser();
-                }            
+                }
             } else if (uri.equals(Settings.System.getUriFor(
                     Settings.System.NAVIGATION_BAR_BUTTON_TINT))
                 || uri.equals(Settings.System.getUriFor(
@@ -655,6 +669,12 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                     mBrightnessView.setVisibility(View.VISIBLE);
                 } else {
                     cleanupBrightnessSlider();
+            } else if (uri.equals(Settings.System.getUriFor(
+                    Settings.System.SHAKE_LISTENER_ENABLED))) {
+                updateShakeListener();
+            } else if (uri.equals(Settings.System.getUriFor(
+                    Settings.System.SHAKE_SENSITIVITY))) {
+                updateShakeSensitivity();
             }
         }
 	updateSettings();
@@ -1303,6 +1323,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         filter.addAction(Intent.ACTION_CLOSE_SYSTEM_DIALOGS);
         filter.addAction(Intent.ACTION_SCREEN_OFF);
         filter.addAction(Intent.ACTION_SCREEN_ON);
+        filter.addAction(Intent.ACTION_USER_PRESENT);
         filter.addAction(ACTION_DEMO);
         filter.addAction(SCHEDULE_REMINDER_NOTIFY);
         filter.addAction(CUSTOM_LOCKSCREEN_STATE);
@@ -1323,6 +1344,8 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
 	mNotificationShortcutsLayout.setupShortcuts();
 
         mNetworkController.setListener(this);
+
+        updateShakeListener();
 
         return mStatusBarView;
     }
@@ -2223,6 +2246,92 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         } else if ((diff & StatusBarManager.DISABLE_NOTIFICATION_TICKER) != 0) {
             if (mTicking && (state & StatusBarManager.DISABLE_NOTIFICATION_TICKER) != 0) {
                 haltTicker();
+            }
+        }
+    }
+
+    private void unregisterShakeListener() {
+        if (mShakeEnabled) {
+            getShakeListener().unregisterShakeListener();
+        }
+    }
+
+    private void registerShakeListener() {
+        if (mShakeEnabled) {
+            getShakeListener().registerShakeListener();
+        }
+    }
+
+    private void updateShakeListener() {
+        mShakeEnabled = Settings.System.getIntForUser(
+                mContext.getContentResolver(),
+                Settings.System.SHAKE_LISTENER_ENABLED, 0,
+                UserHandle.USER_CURRENT) == 1;
+        if (mShakeEnabled) {
+            getShakeListener().setOnShakeListener(new ShakeListener.OnShakeListener() {
+                public void onShake(int direction) {
+                    performShakeEvent(direction);
+                }
+            });
+            updateShakeSensitivity();
+            if ((mScreenOn == null || mScreenOn) && mUserPresent) {
+                mShakeListener.registerShakeListener();
+            }
+        } else {
+            getShakeListener().unregisterShakeListener();
+        }
+    }
+
+    private boolean shakeAppDisabled() {
+        ActivityManager am = (ActivityManager)
+                mContext.getSystemService(Context.ACTIVITY_SERVICE);
+        final String thisPackage = am.getRunningTasks(1)
+                .get(0).topActivity.getPackageName();
+        String resolvedApp = null;
+        boolean shakeDisabled = false;
+        for (ButtonConfig app : ButtonsHelper.getDisabledShakeApps(mContext)) {
+            resolvedApp = app.getClickAction();
+            resolvedApp = resolvedApp.substring(
+                    resolvedApp.lastIndexOf("component=") + 10,
+                    resolvedApp.lastIndexOf("/"));
+            if (resolvedApp.equals(thisPackage)) {
+                shakeDisabled = true;
+            }
+        }
+        return shakeDisabled;
+    }
+
+    private void updateShakeSensitivity() {
+        getShakeListener().setSensitivity(true, Settings.System.getIntForUser(
+                mContext.getContentResolver(),
+                Settings.System.SHAKE_SENSITIVITY, 0, UserHandle.USER_CURRENT));
+    }
+
+    private ShakeListener getShakeListener() {
+        if (mShakeListener == null) {
+            mShakeListener = new ShakeListener(mContext);
+        }
+        return mShakeListener;
+    }
+
+    private void performShakeEvent(int setting) {
+        if (mShakeEnabled && !shakeAppDisabled()) {
+            if (mCurrOrientation == Configuration.ORIENTATION_LANDSCAPE) {
+                // X and Y values are swapped in landscape for ease of
+                // understanding which event will be launched by the user
+                if (setting == 0) {
+                    setting = 1;
+                } else if (setting == 1) {
+                    setting = 0;
+                }
+            }
+            final String event = Settings.System.getStringForUser(
+                    mContext.getContentResolver(),
+                    Settings.System.SHAKE_EVENTS_REGULAR[setting],
+                    UserHandle.USER_CURRENT);
+            if (event != null && !event.equals(ButtonsConstants.ACTION_NULL)) {
+                customButtonVibrate();
+                SlimActions.processAction(mContext, event, false);
             }
         }
     }
@@ -3733,16 +3842,21 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                 animateCollapsePanels(flags);
             } else if (Intent.ACTION_SCREEN_OFF.equals(action)) {
                 mScreenOn = false;
+                mUserPresent = false;
                 // no waiting!
                 makeExpandedInvisible();
                 notifyNavigationBarScreenOn(false);
                 notifyHeadsUpScreenOn(false);
+                unregisterShakeListener();
                 finishBarAnimations();
             } else if (Intent.ACTION_SCREEN_ON.equals(action)) {
                 mScreenOn = true;
                 // work around problem where mDisplay.getRotation() is not stable while screen is off (bug 7086018)
                 repositionNavigationBar();
                 notifyNavigationBarScreenOn(true);
+            } else if (Intent.ACTION_USER_PRESENT.equals(action)) {
+                mUserPresent = true;
+                registerShakeListener();
             } else if (SCHEDULE_REMINDER_NOTIFY.equals(action)) {
                 updateAndNotifyReminder();
             } else if (ACTION_DEMO.equals(action)) {

packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsController.java
@@ -34,8 +34,10 @@ import static com.android.internal.util.mahdi.QSConstants.TILE_LOCKSCREEN;
 import static com.android.internal.util.mahdi.QSConstants.TILE_LTE;
 import static com.android.internal.util.mahdi.QSConstants.TILE_MOBILEDATA;
 import static com.android.internal.util.mahdi.QSConstants.TILE_MUSIC;
+import static com.android.internal.util.mahdi.QSConstants.TILE_NAVBAR;
 import static com.android.internal.util.mahdi.QSConstants.TILE_NETWORKADB;
 import static com.android.internal.util.mahdi.QSConstants.TILE_NETWORKMODE;
+import static com.android.internal.util.mahdi.QSConstants.TILE_NETWORKTRAFFIC;
 import static com.android.internal.util.mahdi.QSConstants.TILE_NFC;
 import static com.android.internal.util.mahdi.QSConstants.TILE_ONTHEGO;
 import static com.android.internal.util.mahdi.QSConstants.TILE_PROFILE;
@@ -44,6 +46,7 @@ import static com.android.internal.util.mahdi.QSConstants.TILE_QUIETHOURS;
 import static com.android.internal.util.mahdi.QSConstants.TILE_RINGER;
 import static com.android.internal.util.mahdi.QSConstants.TILE_SCREENTIMEOUT;
 import static com.android.internal.util.mahdi.QSConstants.TILE_SETTINGS;
+import static com.android.internal.util.mahdi.QSConstants.TILE_SHAKE;
 import static com.android.internal.util.mahdi.QSConstants.TILE_SLEEP;
 import static com.android.internal.util.mahdi.QSConstants.TILE_SYNC;
 import static com.android.internal.util.mahdi.QSConstants.TILE_THEME;
@@ -53,8 +56,6 @@ import static com.android.internal.util.mahdi.QSConstants.TILE_VOLUME;
 import static com.android.internal.util.mahdi.QSConstants.TILE_WIFI;
 import static com.android.internal.util.mahdi.QSConstants.TILE_WIFIAP;
 import static com.android.internal.util.mahdi.QSConstants.TILE_WIMAX;
-import static com.android.internal.util.mahdi.QSConstants.TILE_NAVBAR;
-import static com.android.internal.util.mahdi.QSConstants.TILE_NETWORKTRAFFIC;
 
 import android.content.BroadcastReceiver;
 import android.content.ContentResolver;
@@ -104,6 +105,7 @@ import com.android.systemui.quicksettings.QuietHoursTile;
 import com.android.systemui.quicksettings.RingerModeTile;
 import com.android.systemui.quicksettings.ScreenTimeoutTile;
 import com.android.systemui.quicksettings.SleepScreenTile;
+import com.android.systemui.quicksettings.ShakeEventTile;
 import com.android.systemui.quicksettings.SyncTile;
 import com.android.systemui.quicksettings.ThemeTile;
 import com.android.systemui.quicksettings.ToggleLockscreenTile;
@@ -304,6 +306,8 @@ public class QuickSettingsController {
                 qs = new OnTheGoTile(mContext, this);
             } else if (tile.equals(TILE_BATTERYSAVER)) {
                 qs = new BatterySaverTile(mContext, this);
+            } else if (tile.equals(TILE_SHAKE)) {
+                qs = new ShakeEventTile(mContext, this);
             }
 
             if (qs != null) {