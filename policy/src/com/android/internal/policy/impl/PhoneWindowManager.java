ccd4b819d3d762a4302c6c567470bda6c9f5f6a2
Andy Mast <andy@cyngn.com>
2014-06-06 15:57:58 -0700

    Theme special volumeup+down volume panel
    When you press vol up + vol down button it brings up a
    different volume panel which was not getting themed because
    it lacked the ui context.
    
    Change-Id: I416b8920160c0ab263fd426a6e6491d1f877a905
    
    Conflicts:
    	policy/src/com/android/internal/policy/impl/PhoneWindowManager.java

--- policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
+++ policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
@@ -38,6 +38,7 @@ import android.content.ServiceConnection;
 import android.content.pm.ActivityInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
+import android.content.pm.ThemeUtils;
 import android.content.res.CompatibilityInfo;
 import android.content.res.Configuration;
 import android.content.res.Resources;
@@ -231,6 +232,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     private final Object mQuickBootLock = new Object();
 
     Context mContext;
+    Context mUiContext;
     IWindowManager mWindowManager;
     WindowManagerFuncs mWindowManagerFuncs;
     PowerManager mPowerManager;
@@ -1056,7 +1058,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         public void run() {
             // Do the switch
             final AudioManager am = (AudioManager)mContext.getSystemService(Context.AUDIO_SERVICE);
-            final VolumePanel volumePanel = new VolumePanel(mContext,
+            final int ringerMode = am.getRingerMode();
+            final VolumePanel volumePanel = new VolumePanel(mUiContext,
                                                               (AudioService) getAudioService());
             volumePanel.postVolumeChanged(AudioManager.STREAM_RING,AudioManager.FLAG_SHOW_UI);
         }
@@ -1162,6 +1165,15 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     public void init(Context context, IWindowManager windowManager,
             WindowManagerFuncs windowManagerFuncs) {
         mContext = context;
+        mUiContext = ThemeUtils.createUiContext(context);
+        ThemeUtils.registerThemeChangeReceiver(context, new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                mUiContext = ThemeUtils.createUiContext(mContext);
+            }
+        });
+
+
         mWindowManager = windowManager;
         mWindowManagerFuncs = windowManagerFuncs;
         mHeadless = "1".equals(SystemProperties.get("ro.config.headless", "0"));
