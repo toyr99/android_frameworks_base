9980c4b0fe9070f2bf5ac272510ec265b8621c8d
Mahdi-Rom <mahdi.rom@gmail.com>
2014-06-09 16:14:51 +0100

    SystemUI: remove status bar clock color option

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3011,12 +3011,6 @@ public final class Settings {
         public static final String STATUSBAR_CLOCK_STYLE = "statusbar_clock_style";
 
         /**
-         * Setting for clock color
-         * @hide
-         */
-        public static final String STATUSBAR_CLOCK_COLOR = "statusbar_clock_color";
-
-        /**
         * @hide
         * Shows custom date before clock time
         * 0 - No Date
