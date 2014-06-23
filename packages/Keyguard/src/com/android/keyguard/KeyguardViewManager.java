ada69c57927d24db769060cf96dacf71deca69e6
Mahdi-Rom <mahdi.rom@gmail.com>
2014-06-23 23:09:26 +0100

    Revert "Themes: Keyguard: Fix NPE setting lockscreen wallpaper"
    This reverts commit f65bb1bb444e988589e15b1d8fe7c1395548e38f.

--- packages/Keyguard/src/com/android/keyguard/KeyguardViewManager.java
+++ packages/Keyguard/src/com/android/keyguard/KeyguardViewManager.java
@@ -341,25 +341,20 @@ public class KeyguardViewManager {
                 }
                 d.setColorFilter(BACKGROUND_COLOR, PorterDuff.Mode.SRC_OVER);
                 computeCustomBackgroundBounds(d);
+                Bitmap b = Bitmap.createBitmap(getWidth(), getHeight(), Bitmap.Config.ARGB_8888);
+                Canvas c = new Canvas(b);
+                drawToCanvas(c, d);
 
-                if (isLaidOut()) {
-                    Bitmap b = Bitmap.createBitmap(getWidth(), getHeight(), Bitmap.Config.ARGB_8888);
-                    Canvas c = new Canvas(b);
-                    drawToCanvas(c, d);
+                Drawable dd = new BitmapDrawable(mContext.getResources(), b);
 
-                    Drawable dd = new BitmapDrawable(mContext.getResources(), b);
+                mTransitionBackground = new TransitionDrawable(new Drawable[] {old, dd});
+                mTransitionBackground.setCrossFadeEnabled(true);
+                setBackground(mTransitionBackground);
 
-                    mTransitionBackground = new TransitionDrawable(new Drawable[] {old, dd});
-                    mTransitionBackground.setCrossFadeEnabled(true);
-                    setBackground(mTransitionBackground);
+                mTransitionBackground.startTransition(200);
 
-                    mTransitionBackground.startTransition(200);
+                mCustomBackground = newIsNull ? null : dd;
 
-                    mCustomBackground = newIsNull ? null : dd;
-                } else {
-                    setBackground(d);
-                    mCustomBackground = newIsNull ? null : d;
-                }
             }
             invalidate();
         }
