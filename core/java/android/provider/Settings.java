beb99bde9b4bc1d2f3235a0cd04dd00875e9e0f9
Mahdi-Rom <mahdi.rom@gmail.com>
2014-07-26 06:17:05 +0100

    (1/2) Animation control: remove keyboard animations

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4128,30 +4128,6 @@ public final class Settings {
         public static final String LISTVIEW_DURATION = "listview_duration";
 
         /**
-         *
-         * @hide
-         */
-        public static final String ANIMATION_IME_DURATION = "animation_ime_duration";
-
-        /**
-         *
-         * @hide
-         */
-        public static final String ANIMATION_IME_ENTER = "animation_ime_enter";
-
-        /**
-         *
-         * @hide
-         */
-        public static final String ANIMATION_IME_EXIT = "animation_ime_exit";
-
-        /**
-         *
-         * @hide
-         */
-        public static final String ANIMATION_IME_INTERPOLATOR = "animation_ime_interpolator";
-
-        /**
          * Determine custom scroll friction.
          * @hide
          */
