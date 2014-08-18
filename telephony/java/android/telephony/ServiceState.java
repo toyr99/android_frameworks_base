620667be8c8133a83139ea94229d5dbc3d3cb031
Mahdi-Rom <mahdi.rom@gmail.com>
2014-08-18 08:39:34 +0100

    Revert a lot of telephony stuff
    this reverts:
    e18dac7 Revert "Samsung STK support"
    8513d15 Revert "core and telephony: Additional HSPAP support"
    621bcec Revert "CDMA RIL: add SVDO support (1/2)"
    a784238 Revert "telephony: hide needsOldRilFeature"
    8b4c907 Revert "TelephonyProperties: Alpha ID fix for OPEN_CHANNEL and other p-commands (1/2)"
    72fbaf5 Revert "Enable SVDO. (2/2)"
    5a623d0 Revert "ServiceState: add support for DCHSPAP"
    04d073e Revert "Signal strength: add more checks to signal strength,"
    59fe9c2 Revert "Telephony: fix typo"
    dff07c0 Revert "Telephony: Add support for NETWORK_TYPE_GSM"
    5ec5117 Revert "Telephony: Add TD-SCDMA network modes"
    e9ec74a Revert "SignalStrength: Hotfix TD-SCDMA"
    8db846c Revert "SignalStrength: Hotfix TD-SCDMA take 2"
    e17121b Revert "QS: Add TD-SCDMA support to the LTE toggle tile"
    f5164fb Revert "frameworks/base: forward port DC-HSPAP support from cm-10"
    b505e3a Revert "Telephony: Ignore the timingAdvance field in SignalStrength"

--- telephony/java/android/telephony/ServiceState.java
+++ telephony/java/android/telephony/ServiceState.java
@@ -141,18 +141,11 @@ public class ServiceState implements Parcelable {
     public static final int RIL_RADIO_TECHNOLOGY_LTE = 14;
     /** @hide */
     public static final int RIL_RADIO_TECHNOLOGY_HSPAP = 15;
-    /** @hide */
-    public static final int RIL_RADIO_TECHNOLOGY_DCHSPAP = 30;
     /**
      * GSM radio technology only supports voice. It does not support data.
      * @hide
      */
     public static final int RIL_RADIO_TECHNOLOGY_GSM = 16;
-    /**
-     * TD-SCDMA
-     * @hide
-     */
-    public static final int RIL_RADIO_TECHNOLOGY_TD_SCDMA = 17;
 
     /**
      * Available registration states for GSM, UMTS and CDMA.
@@ -535,16 +528,10 @@ public class ServiceState implements Parcelable {
                 rtString = "LTE";
                 break;
             case RIL_RADIO_TECHNOLOGY_HSPAP:
-                rtString = "HSPA+";
-                break;
-            case RIL_RADIO_TECHNOLOGY_DCHSPAP:
-                rtString = "DC-HSPA+";
+                rtString = "HSPAP";
                 break;
             case RIL_RADIO_TECHNOLOGY_GSM:
                 rtString = "GSM";
-                break;
-            case RIL_RADIO_TECHNOLOGY_TD_SCDMA:
-                rtString = "TD-SCDMA";
                 break;
             default:
                 rtString = "Unexpected";
@@ -706,7 +693,7 @@ public class ServiceState implements Parcelable {
         mOperatorNumeric = m.getString("operator-numeric");
         mIsManualNetworkSelection = m.getBoolean("manual");
         mRilVoiceRadioTechnology = m.getInt("radioTechnology");
-        mRilDataRadioTechnology = m.getInt("dataRadioTechnology");
+        mRilVoiceRadioTechnology = m.getInt("dataRadioTechnology");
         mCssIndicator = m.getBoolean("cssIndicator");
         mNetworkId = m.getInt("networkId");
         mSystemId = m.getInt("systemId");
@@ -810,12 +797,6 @@ public class ServiceState implements Parcelable {
             return TelephonyManager.NETWORK_TYPE_LTE;
         case ServiceState.RIL_RADIO_TECHNOLOGY_HSPAP:
             return TelephonyManager.NETWORK_TYPE_HSPAP;
-        case ServiceState.RIL_RADIO_TECHNOLOGY_GSM:
-            return TelephonyManager.NETWORK_TYPE_GSM;
-        case ServiceState.RIL_RADIO_TECHNOLOGY_TD_SCDMA:
-            return TelephonyManager.NETWORK_TYPE_TD_SCDMA;
-        case ServiceState.RIL_RADIO_TECHNOLOGY_DCHSPAP:
-            return TelephonyManager.NETWORK_TYPE_DCHSPAP;
         default:
             return TelephonyManager.NETWORK_TYPE_UNKNOWN;
         }
@@ -865,9 +846,7 @@ public class ServiceState implements Parcelable {
                 || radioTechnology == RIL_RADIO_TECHNOLOGY_HSPA
                 || radioTechnology == RIL_RADIO_TECHNOLOGY_LTE
                 || radioTechnology == RIL_RADIO_TECHNOLOGY_HSPAP
-                || radioTechnology == RIL_RADIO_TECHNOLOGY_DCHSPAP
-                || radioTechnology == RIL_RADIO_TECHNOLOGY_GSM
-                || radioTechnology == RIL_RADIO_TECHNOLOGY_TD_SCDMA;
+                || radioTechnology == RIL_RADIO_TECHNOLOGY_GSM;
     }
 
     /** @hide */
