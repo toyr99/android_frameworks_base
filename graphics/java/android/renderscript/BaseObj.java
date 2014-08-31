7a033bc0bf24fdc346e2ee38ea6ece2b7bbc13a8
Mahdi-Rom <mahdi.rom@gmail.com>
2014-08-31 22:10:02 +0100

    Revert "Fix null check for BaseObj.equals."
    This reverts commit 98b36f7fdb1b632ed449314db916ad2cb1a0df9c.

--- graphics/java/android/renderscript/BaseObj.java
+++ graphics/java/android/renderscript/BaseObj.java
@@ -181,10 +181,6 @@ public class BaseObj {
         if (this == obj)
             return true;
 
-        if (obj == null) {
-            return false;
-        }
-
         if (getClass() != obj.getClass()) {
             return false;
         }
