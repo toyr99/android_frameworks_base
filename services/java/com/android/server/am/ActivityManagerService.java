55b2dbca6798dab775ba32cd9e79988483bffd4a
Mahdi-Rom <mahdi.rom@gmail.com>
2014-09-14 12:05:33 +0100

    Revert "Add SMS Middleware layer."
    This reverts commit 08fffc43a35bcb5adbd614c46a4bcc3a16fa86ac.

--- services/java/com/android/server/am/ActivityManagerService.java
+++ services/java/com/android/server/am/ActivityManagerService.java
@@ -13289,8 +13289,7 @@ public final class ActivityManagerService extends ActivityManagerNative
                         + " was previously registered for user " + rl.userId);
             }
             BroadcastFilter bf = new BroadcastFilter(filter, rl, callerPackage,
-                    permission, callingUid, userId,
-                    (callerApp != null && callerApp.info != null && (callerApp.info.flags & ApplicationInfo.FLAG_SYSTEM) != 0));
+                    permission, callingUid, userId);
             rl.add(bf);
             if (!bf.debugCheck()) {
                 Slog.w(TAG, "==> For Dynamic broadast");
