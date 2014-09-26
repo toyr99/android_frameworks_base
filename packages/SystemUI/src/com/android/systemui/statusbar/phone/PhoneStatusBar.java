AICP/frameworks_base

MIUI Style carrier text in the statusbar [1/2]
Change-Id: Ib1d8d62b0e575277a779738b9820156e1a3fc999

packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -283,6 +283,9 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
     private boolean mNotificationShortcutsHideCarrier;
     FrameLayout.LayoutParams lpCarrierLabel;
 
+    // Status bar carrier
+    private boolean mShowStatusBarCarrier;
+
     private View mWeatherHeader;
     private boolean mWeatherEnabled = false;
 
@@ -1719,6 +1722,8 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         if ((diff & StatusBarManager.DISABLE_CLOCK) != 0) {
             boolean show = (state & StatusBarManager.DISABLE_CLOCK) == 0;
             showClock(show);
+            //add CarrierLabel
+            showStatusBarCarrierLabel(show);
         }
         if ((diff & StatusBarManager.DISABLE_EXPAND) != 0) {
             if ((state & StatusBarManager.DISABLE_EXPAND) != 0) {
@@ -3175,6 +3180,15 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         }
     }
 
+    public void showStatusBarCarrierLabel(boolean show) {
+        if (mStatusBarView == null) return;
+        ContentResolver resolver = mContext.getContentResolver();
+        View statusBarCarrierLabel = mStatusBarView.findViewById(R.id.status_bar_carrier_label);
+        if (statusBarCarrierLabel != null) {
+            statusBarCarrierLabel.setVisibility(show ? (mShowStatusBarCarrier ? View.VISIBLE : View.GONE) : View.GONE);
+        }
+    }
+
     private void resetUserSetupObserver() {
         mContext.getContentResolver().unregisterContentObserver(mUserSetupObserver);
         mUserSetupObserver.onChange(false);
@@ -3683,6 +3697,9 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                     Settings.System.HEADS_UP_NOTIFCATION_DECAY),
                     false, this, UserHandle.USER_ALL);
 
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.STATUS_BAR_CARRIER), false, this);
+
             updateSettings();
 
         }
@@ -3805,6 +3822,10 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                 Settings.System.CUSTOM_SYSTEM_ICON_COLOR, 0, UserHandle.USER_CURRENT) == 1;
         systemColor = Settings.System.getIntForUser(cr,
                 Settings.System.SYSTEM_ICON_COLOR, -2, UserHandle.USER_CURRENT);
+
+        mShowStatusBarCarrier = Settings.System.getInt(
+            cr, Settings.System.STATUS_BAR_CARRIER, 0) == 1;
+        showStatusBarCarrierLabel(mShowStatusBarCarrier);
 
         updateBatteryIcons();
         updateCustomHeaderStatus();

toyr99/android_frameworks_base

packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -319,6 +319,9 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
     private int mHideLabels;
     private boolean mCarrierAndWifiViewBlocked = false;
 
+    // Status bar carrier
+    private boolean mShowStatusBarCarrier;
+
     // Notification reminder
     private View mReminderHeader;
     private ImageView mSpacer;
@@ -583,6 +586,8 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.STATUS_BAR_CLOCK_LOCKSCREEN), false, this,
                     UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.STATUS_BAR_CARRIER), false, this);
             updateSettings();
         }
 
@@ -4147,7 +4152,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         mNavigationBarView.setLeftInLandscape(navLeftInLandscape);
         }
 
-        mShowAddTileButton = Settings.System.getIntForUser(mContext.getContentResolver(),
+        mShowAddTileButton = Settings.System.getIntForUser(resolver,
                     Settings.System.QUICK_SETTINGS_ADD_TILE_BUTTON, 0, UserHandle.USER_CURRENT) != 0;
         if (mShowAddTileButton && mAddTileButton != null) {
             mAddTileButton.setOnClickListener(mAddTileButtonListener);
@@ -4156,6 +4161,10 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         showClockOnLockscreen = Settings.System.getIntForUser(
                 resolver, Settings.System.STATUS_BAR_CLOCK_LOCKSCREEN, 0
                 , UserHandle.USER_CURRENT) == 1;
+
+        mShowStatusBarCarrier = Settings.System.getInt(
+            resolver, Settings.System.STATUS_BAR_CARRIER, 0) == 1;
+        showStatusBarCarrierLabel(mShowStatusBarCarrier);
 
         mFlipInterval = Settings.System.getIntForUser(mContext.getContentResolver(),
                         Settings.System.REMINDER_ALERT_INTERVAL, 1500, UserHandle.USER_CURRENT);
@@ -4186,6 +4195,15 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                 mCarrierAndWifiView.setVisibility(View.INVISIBLE);
             }
             updateCarrierAndWifiLabelVisibility(false);
+        }
+    }
+
+    public void showStatusBarCarrierLabel(boolean show) {
+        if (mStatusBarView == null) return;
+        ContentResolver resolver = mContext.getContentResolver();
+        View statusBarCarrierLabel = mStatusBarView.findViewById(R.id.status_bar_carrier_label);
+        if (statusBarCarrierLabel != null) {
+            statusBarCarrierLabel.setVisibility(show ? (mShowStatusBarCarrier ? View.VISIBLE : View.GONE) : View.GONE);
         }
     }
 